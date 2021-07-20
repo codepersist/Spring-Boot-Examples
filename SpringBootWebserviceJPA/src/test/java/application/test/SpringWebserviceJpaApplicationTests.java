5c81e57b01741de852146bc5d8d3c909bc576d23
Rahul Mehta <reachme.rahulmehta@gmail.com>
2021-07-20 19:30:15 +0530

    Making change compatible to Boot 2.5 version & JDk 11

--- SpringBootWebserviceJPA/src/test/java/application/test/SpringWebserviceJpaApplicationTests.java
+++ SpringBootWebserviceJPA/src/test/java/application/test/SpringWebserviceJpaApplicationTests.java
@@ -1,15 +1,17 @@
 package application.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(value = { SpringExtension.class })
+@ContextConfiguration(classes = { SpringRunner.class })
 @SpringBootTest
 public class SpringWebserviceJpaApplicationTests {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
